9d6577fa327582ed7142b7fff28c6e8b2eaaaeab
Anizaire Jacky <anizairejacky@gmail.com>
2021-11-12 22:37:19 -0500

    correction des photo de profile

--- font-end/layout/user_bar.php
+++ font-end/layout/user_bar.php
@@ -5,7 +5,6 @@
 		<ul class="c-content-recent-posts-1">
 		<li>
 			<div class="c-image">
-			
 				<img src="<?= $link_admin ?>/dist/img/user/participant/<?= Fonctions::user()->photo ?>" onerror="this.src='https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=20&m=1214428300&s=170667a&w=0&h=NPyJe8rXdOnLZDSSCdLvLWOtIeC9HjbWFIx8wg5nIks='"" alt="" class="img-responsive img-thumbnail">
 			</div>
 			<div class="c-post">
--- font-end/layout/user_image.php
+++ font-end/layout/user_image.php
@@ -1,10 +1,6 @@
 <!-- <button>Connexoin</button> -->
 
 <?php if(isset($_SESSION['id_user'])): ?>
-<img src="<?= $link_admin ?>/dist/img/user/participant/<?= Fonctions::user()->photo ?>" class="img-responsive img-circle">
+<img src="<?= $link_admin ?>/dist/img/user/participant/<?= Fonctions::user()->photo ?>" onerror="this.src='https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=20&m=1214428300&s=170667a&w=0&h=NPyJe8rXdOnLZDSSCdLvLWOtIeC9HjbWFIx8wg5nIks='"
+ class="img-responsive img-circle">
 <?php endif ?>
-
-src="<?= $link_admin ?>/dist/img/user/participant/<?= Fonctions::user()->photo ?>" onerror="this.src='https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=20&m=1214428300&s=170667a&w=0&h=NPyJe8rXdOnLZDSSCdLvLWOtIeC9HjbWFIx8wg5nIks='"
-
-
-// <img src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=20&m=1214428300&s=170667a&w=0&h=NPyJe8rXdOnLZDSSCdLvLWOtIeC9HjbWFIx8wg5nIks=" class="img-responsive img-circle">
